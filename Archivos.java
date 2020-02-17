c3425926a5c358554dcd908e675d80539c2cb733
campeon19 <53914171+campeon19@users.noreply.github.com>
2020-02-16 20:19:57 -0600

    archivos txt

---
+++ Archivos.java
@@ -0,0 +1,95 @@
+/*
+ * Christian Daniel Perez De Leon
+ * 
+ * fuentes consultadas: http://decodigo.com/
+ */
+
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+
+public class Archivos {
+
+	public int[] numerosAzar(int cantidad, int maximo) {
+		int[] x = new int[cantidad];
+		for(int i=0; i < cantidad; i++) {
+			x[i] = (int)(Math.random()* maximo+1);
+		}
+		
+		return x;
+	}
+	
+	
+	public void escribir(int[] numeros) {
+		
+		try {
+			File file = new File("numeros.txt");
+			
+			
+			if(!file.exists()) {
+				file.createNewFile();
+			}
+			
+			FileWriter writer = new FileWriter(file);
+			BufferedWriter bwriter = new BufferedWriter(writer);
+			
+			for (int i: numeros) {
+				bwriter.write(i + ",");
+				
+			}
+			
+			bwriter.close();
+			writer.close();
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+			
+	}
+	
+	
+	public Integer[] leer() {
+		
+		Integer[] x = null;
+		
+		try {
+			
+			File file = new File("numeros.txt");
+			FileReader reader = new FileReader(file);
+			BufferedReader breader = new BufferedReader(reader);
+			String a = "";
+			String b = "";
+			
+			while((b = breader.readLine()) != null) {
+				a = b;
+			}
+			
+			
+			String[] separar = a.split(",");
+			x = new Integer[separar.length];
+			
+			for(int i=0; i< separar.length -1; i++) {
+				
+				x[i] = Integer.parseInt(separar[i]);
+			}
+			
+			breader.close();
+			reader.close();
+			
+			
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return x;
+	}
+	
+	
+}
